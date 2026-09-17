3998be5f70cd79a6bd57a38e1661dd9ec87254ef
primularossa94117-gif <primularossa94117@gmail.com>
2026-09-17 13:15:37 +0200

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-/* ------------------------------
+document.addEventListener("DOMContentLoaded", () => {
+   /* ------------------------------
    CAMBIO PAGINE
 ------------------------------ */
 function showPage(pageId) {
